0f985a89ccbe61b04ce3ed69e653f27e7bf9314b
thomasschafer <thomasschafer97@gmail.com>
2024-07-11 22:19:16 +0100

    Add first 15 exercises

--- exercises/008_quiz.zig
+++ exercises/008_quiz.zig
@@ -10,33 +10,14 @@
 const std = @import("std");
 
 pub fn main() void {
-    // What is this nonsense? :-)
     const letters = "YZhifg";
 
-    // Note: usize is an unsigned integer type used for...sizes.
-    // The exact size of usize depends on the target CPU
-    // architecture. We could have used a u8 here, but usize is
-    // the idiomatic type to use for array indexing.
-    //
-    // There IS a problem on this line, but 'usize' isn't it.
-    const x: usize = 1;
-
-    // Note: When you want to declare memory (an array in this
-    // case) without putting anything in it, you can set it to
-    // 'undefined'. There is no problem on this line.
     var lang: [3]u8 = undefined;
 
-    // The following lines attempt to put 'Z', 'i', and 'g' into the
-    // 'lang' array we just created by indexing the array
-    // 'letters' with the variable 'x'. As you can see above, x=1
-    // to begin with.
-    lang[0] = letters[x];
-
-    x = 3;
-    lang[???] = letters[x];
-
-    x = ???;
-    lang[2] = letters[???];
+    for (0..3) |i| {
+        const x = 2 * i + 1;
+        lang[i] = letters[x];
+    }
 
     // We want to "Program in Zig!" of course:
     std.debug.print("Program in {s}!\n", .{lang});
